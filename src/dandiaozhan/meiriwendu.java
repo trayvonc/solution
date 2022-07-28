89bb6d2f5baf934c510a4c98387b763ea35e9ea9
trayvon <trayvon@yeah.net>
2022-07-28 08:47:28 +0800

    Changes

---
+++ src/dandiaozhan/meiriwendu.java
@@ -0,0 +1,21 @@
+package dandiaozhan;
+
+import java.util.Stack;
+
+public class meiriwendu {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack=new Stack<>();
+        int[] ans=new int[temperatures.length];
+        stack.push(0);
+        for(int i=1;i<temperatures.length;i++){
+            //依次和栈顶比较，由于要找到第一个比自己大的，所以要从顶到底部升序，顶部最小
+            while(!stack.isEmpty()&&temperatures[i]>temperatures[stack.peek()]){//栈不空，且大于，说明找到一第一个比我大的了
+                ans[stack.peek()]=i-stack.peek();
+                stack.pop();
+            }
+            stack.push(i);//如果小于等于，直接入栈
+
+        }
+        return ans;
+    }
+}
