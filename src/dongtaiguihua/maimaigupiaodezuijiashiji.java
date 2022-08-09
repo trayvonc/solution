0008896b71568a4dd5706520e023cf2fb67503de
trayvon <trayvon@yeah.net>
2022-08-09 23:24:22 +0800

    Changes

--- src/dongtaiguihua/maimaigupiaodezuijiashiji.java
+++ src/dongtaiguihua/maimaigupiaodezuijiashiji.java
@@ -1,4 +1,18 @@
 package dongtaiguihua;
 
 public class maimaigupiaodezuijiashiji {
+    //dp[i][0]第i天持有股票所得最多现金
+    //dp[i][1]第i天不持有股票所得最多现金
+    public int maxProfit(int[] prices) {
+        int[][] dp=new int[prices.length][2];
+        dp[0][0]-=prices[0];
+        dp[0][1]=0;
+        for(int i=1;i<prices.length;i++){
+            dp[i][0]=Math.max(dp[i-1][0],-prices[i]);
+            dp[i][1]=Math.max(dp[i-1][1],dp[i-1][0]+prices[i]);
+        }
+        return dp[prices.length-1][1];
+    }
+
+
 }
