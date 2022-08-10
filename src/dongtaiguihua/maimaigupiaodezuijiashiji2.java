72310d2b02d05cad88660954ed1d31db92935f6e
trayvonc <trayvon@yeah.net>
2022-08-10 17:02:50 +0800

    caic

---
+++ src/dongtaiguihua/maimaigupiaodezuijiashiji2.java
@@ -0,0 +1,17 @@
+package dongtaiguihua;
+
+public class maimaigupiaodezuijiashiji2 {
+    public int maxProfit(int[] prices) {
+        int[][] dp=new int[prices.length][2];
+        //持有的现金
+        dp[0][0]-=prices[0];
+        //不持有的现金
+        dp[0][1]=0;
+        for(int i=1;i<prices.length;i++){
+            dp[i][0]=Math.max(dp[i-1][0],dp[i-1][1]-prices[i]);//布置买一次，可以是上一次的利润再卖
+            dp[i][1]=Math.max(dp[i-1][1],dp[i-1][0]+prices[i]);
+        }
+        return dp[prices.length-1][1];
+
+    }
+}
