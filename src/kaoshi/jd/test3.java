9972819f7fa3963d11e4b31a5e54339a56d2f0e4
trayvon <trayvon@yeah.net>
2022-08-27 21:28:11 +0800

    Changes

--- src/kaoshi/jd/test3.java
+++ src/kaoshi/jd/test3.java
@@ -8,17 +8,15 @@ public class test3 {
         int aa=s.nextInt();
         int max=0;
         int x=aa-6;
-        int res=0;
         for(int a=0;a<=x;a++){
             for(int b=0;b<=x;b++){
                 int c=x-a-b;
                 if(c>=0&&c<=x){
                     max++;
                     System.out.println(a+","+b+","+c);
-                    res+=Math.pow(26,a)+Math.pow(26,b)+Math.pow(26,c);
                 }
             }
         }
-        System.out.println((res)%(1000000007));
+        System.out.println((int)(max*Math.pow(26,x))%(1000000007));
     }
 }
